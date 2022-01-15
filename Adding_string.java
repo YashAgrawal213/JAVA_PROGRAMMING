1db42e22c93d34072a39d65c08e7e46ca1bc9fbb
Yash Agrawal <80036700+YashAgrawal213@users.noreply.github.com>
2022-01-15 16:04:53 +0530

    adding strings

---
+++ Adding_string.java
@@ -0,0 +1,9 @@
+public class Adding_string {
+    public static void main(String[] args) {
+        String firstName = "John";
+        String lastName = " doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+    }
+}
+
