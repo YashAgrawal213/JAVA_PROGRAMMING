f4bbbe6287aab1cb2f19fbbb05cc05eef7af26cd
Yash Agrawal <80036700+YashAgrawal213@users.noreply.github.com>
2022-08-06 23:30:29 +0530

    Add_Three_Numbers

---
+++ Add_Three_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Add_Three_Numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        int a = x+y+z;
+        System.out.println(a);
+    }
+}
