d24e293e138e583dad782363f82ba08d9c3764c5
Yash Agrawal <80036700+YashAgrawal213@users.noreply.github.com>
2022-08-07 19:36:29 +0530

    Array_Insertion_At_Any_Index_Location

---
+++ Array_Insertion_At_Any_Index_Location.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Array_Insertion_At_Any_Index_Location
+{
+    public static void main(String[] args)
+    {
+        int n,m,p;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter no. of elements in array");
+        n=sc.nextInt();
+        int a[]= new int[n];
+        int b[]= new int[n+1];
+        System.out.println("enter values");
+        for (int i = 0; i < n; i++)
+        {
+            a[i]= sc.nextInt();
+        }
+        System.out.println("enter index where value to be inserted");
+        m=sc.nextInt();
+        System.out.println("enter new value to be inserted");
+        p=sc.nextInt();
+        for (int i = 0; i < (n+1); i++) {
+            if (i<m)
+            {
+                b[i]= a[i];
+            }
+            else if (i==m)
+            {
+                b[i]=p;
+            }
+            else
+            {
+                b[i]= a[i-1];
+            }
+        }
+        System.out.println("elements are:");
+        for (int i = 0; i < (n+1); i++)
+        {
+            System.out.println(b[i]);
+        }
+    }
+}
